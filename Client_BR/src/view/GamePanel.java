689435b58645650798e836824d07a9b86b2aaae5
Padiooo <clement.perin@viacesi.fr>
2019-10-05 20:12:18 +0200

    BR_Client v1.0
    Can launch one single game, need quit and restart application for another game.

--- Client_BR/src/view/GamePanel.java
+++ Client_BR/src/view/GamePanel.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
@@ -10,10 +13,59 @@ public class GamePanel extends JPanel {
 	private int size_x;
 	private int size_y;
 
+	private Color blank = Color.GRAY;
+	private Color color_player = Color.WHITE;
+	private Color color_ennemy = Color.BLACK;
+
+	private int[][][] data;
+	private int[] players_id;
+
+	private int id_player;
+	private int size_player;
+	private int size_ball;
+
 	public GamePanel(int client_id, int size_x, int size_y) {
 		this.client_id = client_id;
 		this.size_x = size_x;
 		this.size_y = size_y;
+	}
+
+	public void paintComponent(Graphics g) {
+
+		g.clearRect(0, 0, size_x, size_y);
+		g.setColor(blank);
+		g.fillRect(0, 0, size_x, size_y);
+
+		if (data != null) {
+			for (int id_player = 0; id_player < data.length; id_player++) {
+				if (this.id_player != players_id[id_player]) {
+					g.setColor(color_ennemy);
+				} else {
+					g.setColor(color_player);
+				}
+				for (int id_player_ball = 0; id_player_ball < data[0].length; id_player_ball++) {
+					if (data[id_player][id_player_ball][0] != -99) {
+						if (id_player_ball != 0) {
+							//@formatter:off
+							g.fillOval(data[id_player][id_player_ball][0] - size_ball / 2, data[id_player][id_player_ball][1] - size_ball / 2, size_ball, size_ball);
+							//@formatter:on
+						} else {
+							//@formatter:off
+							g.fillOval(data[id_player][id_player_ball][0] - size_player / 2, data[id_player][id_player_ball][1] - size_player / 2, size_player, size_player);
+							//@formatter:on
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public void setData(int[][][] data) {
+		this.data = data;
+	}
+
+	public void setId(int[] players_id) {
+		this.players_id = players_id;
 	}
 
 }
